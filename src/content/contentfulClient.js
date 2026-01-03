0e06c19ece8ff38a34e3db87012457865083416f
matcbv <matheuscbv23@gmail.com>
2026-01-03 19:57:05 -0300

    Remocao do arquvio com credencial do Contentful e atualizacao do gitignore

--- src/content/contentfulClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'contentful';
-
-export const client = createClient({
-	space: 'y60vdtprw3sq',
-	environment: 'master',
-	accessToken: '4G7L1oV5TJRhKPd-5Q5-hJ_jBbHK76npaQlKy2dM64w',
-});
