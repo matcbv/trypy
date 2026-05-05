17ce454f9eaef4fbd1dc08cb7efcbb14554c56a2
matcbv <matheuscbv23@gmail.com>
2026-05-05 04:52:17 -0300

    Adicao de plugin contendo regras gerais do React

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginReact from 'eslint-plugin-react';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
@@ -19,6 +20,7 @@ export default defineConfig([
 			},
 		},
 		plugins: {
+			react: pluginReact,
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 		},
