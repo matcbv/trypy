8900aa50195c42a7e2248e6db1c3a2d929c72aac
matcbv <matheuscbv23@gmail.com>
2026-06-11 03:30:27 -0300

    Implementacao de diferentes formas de exclusao de conta dependendo da forma de autenticacao

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthContext } from '../contexts/AuthProvider/context';
 import { ProfileForm } from '../components/ProfileForm';
 import { deleteAccount } from '../database/auth/auth';
@@ -12,24 +12,44 @@ import { storageKeys } from '../constants/storageKeys';
 import { useSafeContext } from '../hooks/useSafeContext';
 import type { ToastData } from '../types/toast';
 import { FirebaseError } from 'firebase/app';
+import { auth } from '../database/configs/firebase';
+import { getIdTokenResult, ProviderId } from 'firebase/auth';
+import { idGenerator } from '../utils/idGenerator';
 
 export function EditProfile() {
+	type Providers = (typeof ProviderId)[keyof typeof ProviderId];
+
 	const navigate = useNavigate();
 	const { authState, authDispatch } = useSafeContext(AuthContext);
 	const { progressDispatch } = useSafeContext(ProgressContext);
 	const [userPassword, setUserPassword] = useState('');
 	const [isVisible, setIsVisible] = useState(false);
+	const [provider, setProvider] = useState<Providers | null>(null);
+	const [deleteInputValue, setDeleteInputValue] = useState<string>('');
+	const [deleteCode] = useState(() => idGenerator().generateID());
+
+	useEffect(() => {
+		void (async () => {
+			const { signInProvider } = await getIdTokenResult(auth.currentUser!);
+			setProvider(signInProvider as Providers);
+		})();
+	});
 
 	const deleteAccountWrapper = async () => {
-		if (userPassword.length <= 0) return;
 		try {
-			await deleteAccount(userPassword, authState.uid!);
+			if (provider === ProviderId.PASSWORD) {
+				if (userPassword.length <= 0) return;
+				await deleteAccount({ uid: authState.uid!, password: userPassword });
+			} else {
+				if (!provider || deleteCode !== deleteInputValue) return;
+				await deleteAccount({ uid: authState.uid!, provider: provider });
+			}
 
 			authDispatch({ type: authActionTypes.LOGOUT });
 			progressDispatch({ type: progressActionTypes.RESET_PROGRESS });
 			localStorage.removeItem(storageKeys.NAVIGATION_STATE);
 
-			void navigate('/');
+			void navigate('/', { replace: true });
 			toast<ToastData>(ToastNotification, {
 				type: 'success',
 				data: {
@@ -56,6 +76,39 @@ export function EditProfile() {
 		}
 	};
 
+	const deleteField =
+		provider === ProviderId.PASSWORD ? (
+			<div className="relative flex w-[300px] items-center">
+				<input
+					value={userPassword}
+					placeholder="Senha atual"
+					onChange={(e) => setUserPassword(e.target.value)}
+					className="w-[300px] rounded-md border-2 border-[var(--main-purple)]/60 bg-white/5 py-2 pr-9 pl-3 text-sm transition-shadow outline-none focus:border-[var(--main-purple)] focus:shadow-[0_0_5px_#ffffff1f]"
+					type={isVisible ? 'text' : 'password'}
+				/>
+				<img
+					src={`/assets/images/icons/${isVisible ? 'hide' : 'visible'}.png`}
+					alt={isVisible ? 'Esconder senha' : 'Exibir senha'}
+					className="absolute right-3 cursor-pointer"
+					onClick={() => setIsVisible((prev) => !prev)}
+				/>
+			</div>
+		) : (
+			<>
+				<p className="text-sm">
+					Digite o código gerado para concluir a exclusão da conta:{' '}
+					<span className="font-bold text-red-300">{deleteCode}</span>
+				</p>
+				<input
+					value={deleteInputValue}
+					placeholder="Código de exclusão"
+					onChange={(e) => setDeleteInputValue(e.target.value)}
+					className="w-[300px] rounded-md border-2 border-[var(--main-purple)]/60 bg-white/5 py-2 pr-9 pl-3 text-sm transition-shadow outline-none focus:border-[var(--main-purple)] focus:shadow-[0_0_5px_#ffffff1f]"
+					type="text"
+				/>
+			</>
+		);
+
 	return (
 		<div>
 			<h1 className="mb-10 text-3xl font-bold tracking-wide">Meus dados</h1>
@@ -70,31 +123,18 @@ export function EditProfile() {
 						Alterar senha
 					</Link>
 				</div>
-				<div>
-					<h3 className="mb-3">Deletar conta</h3>
-					<p className="mb-4 text-sm">
-						Essa ação é <span className="font-bold">irreversível</span>. Não é
+				<div className="flex flex-col gap-y-3">
+					<h3>Deletar conta</h3>
+					<p className="text-sm">
+						Essa ação é{' '}
+						<span className="font-bold text-red-300">irreversível</span>. Não é
 						possível recuperar a conta após sua exclusão.
 					</p>
-					<div className="flex flex-col items-start gap-y-3">
-						<div className="relative flex items-center">
-							<input
-								value={userPassword}
-								placeholder="Senha atual"
-								onChange={(e) => setUserPassword(e.target.value)}
-								className="w-[300px] rounded-md border-2 border-[var(--main-purple)]/60 bg-white/5 py-2 pr-9 pl-3 text-sm transition-shadow outline-none focus:border-[var(--main-purple)] focus:shadow-[0_0_5px_#ffffff1f]"
-								type={isVisible ? 'text' : 'password'}
-							/>
-							<img
-								src={`/assets/images/icons/${isVisible ? 'hide' : 'visible'}.png`}
-								alt={isVisible ? 'Esconder senha' : 'Exibir senha'}
-								className="absolute right-3 cursor-pointer"
-								onClick={() => setIsVisible((prev) => !prev)}
-							/>
-						</div>
+					<div className="flex flex-col gap-y-3">
+						{deleteField}
 						<button
 							type="button"
-							className={`form-btn w-[120px] text-sm ${userPassword.length > 0 ? 'cursor-pointer' : 'cursor-not-allowed'}`}
+							className="form-btn w-[120px] cursor-pointer text-sm"
 							onClick={() => void deleteAccountWrapper()}
 						>
 							Continuar
