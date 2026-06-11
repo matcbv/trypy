d73ea49d223bafda615948e4e8948338c07c2b71
matcbv <matheuscbv23@gmail.com>
2026-06-11 02:08:53 -0300

    Criacao de constante contendo os temas do react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { ToastContainer } from 'react-toastify';
 
 // Routes
-import { AppRoutes } from './routes';
+import { AppRoutes } from './routes/AppRoutes';
 
 // Styles
 import './assets/fonts.css';
 import './assets/tailwind.css';
 import './assets/styles.css';
+
+const toastThemes = {
+	success: '!shadow-[0_0_15px_#00803e50]',
+	error: '!shadow-[0_0_15px_#f2485450]',
+	warning: '!shadow-[0_0_15px_#f7b97250]',
+	info: '!shadow-[0_0_15px_#5c87a750]',
+	default: '',
+};
 
 function App() {
 	return (
@@ -19,9 +27,7 @@ function App() {
 				pauseOnHover={false}
 				pauseOnFocusLoss={false}
 				toastClassName={(context) =>
-					(context?.type === 'success'
-						? '!shadow-[0_0_15px_#00803e50]'
-						: '!shadow-[0_0_15px_#f2485450]') +
+					toastThemes[context?.type || 'default'] +
 					' relative mb-4 !w-[400px] rounded-lg !bg-[#131117eb] !p-6'
 				}
 				className="mt-20"
