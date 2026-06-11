a76e7e07471a624d5bf64e0339efa43f98957faf
matcbv <matheuscbv23@gmail.com>
2026-06-11 02:23:47 -0300

    Alteracao na estrutura do popup do react-toastify

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,26 +1,17 @@
 import type { ToastContentProps } from 'react-toastify';
 import type { ToastData } from '../types/toast';
 
-export function ToastNotification({
-	closeToast,
-	data,
-}: ToastContentProps<ToastData>) {
+export function ToastNotification({ data }: ToastContentProps<ToastData>) {
 	return (
 		<div className="flex items-center gap-x-3">
 			<img
 				src={`/assets/images/icons/${data.type}.png`}
 				alt={data.type}
-				className="w-[30px]"
+				className="w-[25px]"
 			/>
-			<p className="font-jetbrains text-sm leading-relaxed font-bold text-white">
+			<p className="font-jetbrains text-sm leading-6 font-bold text-white">
 				{data.text}
 			</p>
-			<img
-				src="/assets/images/icons/close.png"
-				alt="Fechar"
-				className="absolute top-2 right-2 w-[18px] cursor-pointer"
-				onClick={() => closeToast()}
-			/>
 		</div>
 	);
 }
