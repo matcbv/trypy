8a08654accc1d3e4b371a447d8d8e9316b127707
matcbv <matheuscbv23@gmail.com>
2026-06-17 10:57:34 -0300

    Mudanca do uso de classes do tailwind

--- src/components/ModuleCard.tsx
+++ src/components/ModuleCard.tsx
@@ -48,7 +48,7 @@ export function ModuleCard({ card }: { card: ModuleCardData }) {
 			key={card.title}
 			className="relative h-[460px] w-[467px] overflow-hidden rounded-md bg-[url('/assets/images/batthern.png')] shadow-[0_0_20px_#000000a1]"
 		>
-			<div className="flex h-full w-full flex-col bg-[#0f0d16dc]">
+			<div className="flex size-full flex-col bg-[#0f0d16dc]">
 				<h2 className="flex gap-x-3 rounded-t-md border-b border-(--card-color) p-5">
 					{card.title}
 					{progressState.doneModules.includes(card.moduleId) && (
--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,6 +1,6 @@
 export function LoadingPage() {
 	return (
-		<div className="absolute top-0 left-0 flex h-full w-full items-center justify-center rounded-lg bg-black/20 backdrop-blur-3xl">
+		<div className="absolute top-0 left-0 flex size-full items-center justify-center rounded-lg bg-black/20 backdrop-blur-3xl">
 			<img
 				className="relative w-20"
 				src="/assets/images/loading.png"
