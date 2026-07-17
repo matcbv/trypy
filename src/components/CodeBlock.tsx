1549069f6f67853a032f7f09a4fe2dfff803263f
matcbv <matheuscbv23@gmail.com>
2026-07-17 20:10:13 -0300

    Mudancas na estilizacao do terminal

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -5,7 +5,14 @@ export function CodeBlock({ code }: { code: string }) {
 	const copyIcon = useRef<HTMLImageElement>(null);
 	const [isCopied, setIsCopied] = useState(false);
 
-	const html = useMemo(() => highlightCode(code), [code]);
+	const html = useMemo(
+		() =>
+			highlightCode(code).replace(
+				/>(\.\.\.|>>>)/g,
+				'><span class="text-[#81c8be] select-none">$1 </span>',
+			),
+		[code],
+	);
 
 	const copyText = async (text: string) => {
 		await navigator.clipboard.writeText(text);
@@ -16,10 +23,7 @@ export function CodeBlock({ code }: { code: string }) {
 	};
 
 	return (
-		<div
-			className="font-jetbrains group relative cursor-pointer rounded text-sm shadow-[0_0_10px_var(--color-main-purple)]/15"
-			onClick={() => void copyText(code)}
-		>
+		<div className="font-jetbrains group relative rounded text-sm shadow-[0_0_10px_var(--color-main-purple)]/15">
 			<div
 				className="overflow-hidden rounded-lg"
 				dangerouslySetInnerHTML={{ __html: html }}
@@ -29,6 +33,7 @@ export function CodeBlock({ code }: { code: string }) {
 				src={`${isCopied ? '/assets/images/icons/success.png' : '/assets/images/icons/copy.png'}`}
 				alt="Copiar código"
 				className="absolute top-3 right-3 w-5 scale-0 cursor-pointer transition-transform group-hover:scale-100"
+				onClick={() => void copyText(code)}
 			/>
 		</div>
 	);
