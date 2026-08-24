5265803ce3dbab9d471a2d3a1ab87498d90f2db1
matcbv <matheuscbv23@gmail.com>
2026-08-24 00:44:17 -0300

    Configuracao do mkcert no vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		mkcert({
+			savePath: './.mkcert',
+		}),
+	],
 	optimizeDeps: {
 		exclude: ['pyodide'],
+	},
+	server: {
+		host: true,
 	},
 });
