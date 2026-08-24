b249e9efe41f462f7af4781af990afe916be93b0
matcbv <matheuscbv23@gmail.com>
2026-08-24 00:08:06 -0300

    Aplicacao de responsividade no formulario de cadastro

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -160,7 +160,7 @@ export function RegisterForm() {
 						id="birthDate"
 						onChange={handleChange}
 						value={userData.birthDate}
-						className={`focus:border-main-green w-[300px] rounded-full border-2 border-white/30 bg-white/5 py-1 pr-9 pl-3 tracking-wider transition-colors duration-300 outline-none ${!placeholders.birthDate ? 'placeholder-gray-400' : 'placeholder-red-400'}`}
+						className={`focus:border-main-green w-full rounded-full border-2 border-white/30 bg-white/5 py-1 pr-9 pl-3 tracking-wide transition-colors duration-300 outline-none ${!placeholders.birthDate ? 'placeholder-gray-400' : 'placeholder-red-400'}`}
 					/>
 				);
 			default:
@@ -171,7 +171,7 @@ export function RegisterForm() {
 						onChange={handleChange}
 						value={userData[key]}
 						placeholder={placeholders[key]}
-						className="focus:border-main-green w-[300px] rounded-full border-2 border-white/30 bg-white/5 py-1 pr-9 pl-3 placeholder-red-400 transition-colors duration-300 outline-none"
+						className="focus:border-main-green w-full rounded-full border-2 border-white/30 bg-white/5 py-1 pr-9 pl-3 placeholder-red-400 transition-colors duration-300 outline-none"
 					/>
 				);
 		}
@@ -181,64 +181,69 @@ export function RegisterForm() {
 		<LoadingPage />
 	) : (
 		<form
-			className="flex h-full flex-col justify-center"
+			className="flex flex-col gap-x-10 md:flex-row md:items-center"
 			onSubmit={(e) => void handleSubmit(e)}
 		>
-			<div className="flex justify-between">
-				<div className="flex h-full flex-col justify-center">
-					<div className="flex flex-col gap-y-3">
-						{(
-							Object.entries(formMap) as Array<[keyof typeof formMap, string]>
-						).map(([key, value]) => (
-							<div className="flex flex-col" key={key}>
-								<label
-									htmlFor={key}
-									className="font-jetbrains ml-3 text-sm font-bold"
-								>
-									{value}
-								</label>
-								{inputType(key)}
-							</div>
-						))}
-					</div>
+			<div className="mb-4 flex h-full flex-col justify-center md:mb-0">
+				<div className="flex flex-col gap-y-2 md:gap-y-4">
+					{(
+						Object.entries(formMap) as Array<[keyof typeof formMap, string]>
+					).map(([key, value]) => (
+						<div className="flex flex-col" key={key}>
+							<label
+								htmlFor={key}
+								className="font-jetbrains ml-3 text-sm font-bold"
+							>
+								{value}
+							</label>
+							{inputType(key)}
+						</div>
+					))}
 				</div>
-				<div className="flex h-full flex-col justify-center gap-y-6">
-					<p className="font-space-grotesk w-[303px] text-center text-4xl leading-14 tracking-wide text-shadow-[5px_5px_10px_#000000]/80">
-						O{' '}
-						<span className="decoration-main-green underline decoration-2 underline-offset-10">
-							primeiro passo
-						</span>{' '}
-						é sempre o mais{' '}
-						<span className="text-main-purple text-5xl tracking-wider italic">
-							importante
-						</span>
-					</p>
-					<div className="flex flex-col items-center gap-y-4">
-						<span
-							className="register-form-btn border-main-purple flex items-center justify-center gap-x-2"
-							onClick={() => void handleGoogle()}
-						>
-							<p className="text-lg">Google</p>
-							<img
-								className="h-8 w-8"
-								src="/assets/images/icons/google.png"
-								alt="Google"
-							/>
-						</span>
-						<span className="register-form-btn border-main-purple flex items-center justify-center gap-x-2">
-							<p className="text-lg">GitHub</p>
-							<img
-								className="h-8 w-8"
-								src="/assets/images/icons/github.png"
-								alt="GitHub"
-							/>
-						</span>
-						<input
-							type="submit"
-							value="Continuar"
-							className="register-form-btn border-main-green"
-						/>
-					</div>
+			</div>
+			<div className="flex flex-col gap-y-4">
+				<p className="font-space-grotesk hidden w-[303px] text-center text-4xl leading-14 tracking-wide text-shadow-[5px_5px_10px_#000000]/80 md:block">
+					O{' '}
+					<span className="decoration-main-green underline decoration-2 underline-offset-10">
+						primeiro passo
+					</span>{' '}
+					é sempre o mais{' '}
+					<span className="text-main-purple text-5xl tracking-wider italic">
+						importante
+					</span>
+				</p>
+				<div className="flex flex-row-reverse items-center justify-between gap-y-4 md:flex-col">
+					<button
+						className="register-form-btn border-main-purple hidden items-center justify-center gap-x-2 md:flex"
+						onClick={() => void handleGoogle()}
+					>
+						<p className="text-lg">Google</p>
+						<img src="/assets/images/icons/google.png" alt="Google" />
+					</button>
+					<img
+						className="cursor-pointer transition-transform duration-300 hover:scale-110 md:hidden"
+						src="/assets/images/icons/google.png"
+						alt="Google"
+						role="button"
+						tabIndex={0}
+						onClick={() => void handleGoogle()}
+					/>
+					<button className="register-form-btn border-main-purple hidden items-center justify-center gap-x-2 md:flex">
+						<p className="block text-lg">GitHub</p>
+						<img src="/assets/images/icons/github.png" alt="GitHub" />
+					</button>
+					<img
+						className="cursor-pointer transition-transform duration-300 hover:scale-110 md:hidden"
+						src="/assets/images/icons/github.png"
+						alt="Google"
+						role="button"
+						tabIndex={0}
+					/>
+					<input
+						type="submit"
+						value="Continuar"
+						className="register-form-btn border-main-green"
+					/>
 				</div>
 			</div>
 		</form>
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,10 +3,10 @@ import { RegisterForm } from '../components/RegisterForm';
 export function Register() {
 	return (
 		<div className="flex min-h-screen items-center justify-center">
-			<div className="to-main-bg relative my-20 rounded-lg bg-linear-to-br from-[#12612f] via-[#240051] shadow-[0_0_20px_#ffffff]/10">
-				<div className='absolute inset-0 rounded-lg bg-[url("/assets/images/cartographer.png")] opacity-15 backdrop-blur-3xl' />
-				<div className="relative flex h-[520px] w-[720px] flex-col gap-y-5 rounded-lg p-10">
-					<h1 className="text-3xl font-bold tracking-wider">Criar conta</h1>
+			<div className="to-main-bg relative mx-[10px] my-[120px] flex min-h-[510px] w-full max-w-[380px] rounded-lg bg-linear-to-br from-[#12612f] via-[#240051] shadow-[0_0_20px_#ffffff]/10 transition-[max-width] duration-500 md:max-w-[723px]">
+				<div className='absolute inset-0 rounded-lg bg-[url("/assets/images/cartographer.png")] opacity-20'></div>
+				<div className="relative flex w-full flex-col gap-y-5 rounded-lg p-10">
+					<h1 className="text-3xl font-bold tracking-wide">Criar conta</h1>
 					<RegisterForm />
 				</div>
 			</div>
