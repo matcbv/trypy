3c06d9692cfb645ebba12f2aecdbe710066613ca
matcbv <matheuscbv23@gmail.com>
2026-09-12 17:13:13 -0300

    Funcoes de controle do shiki passadas para um unico arquivo

--- src/lib/shiki.ts
+++ src/lib/shiki.ts
@@ -1,6 +1,13 @@
 import { createHighlighter } from 'shiki';
 
-export const highlighter = await createHighlighter({
+const highlighter = await createHighlighter({
 	themes: ['catppuccin-frappe'],
 	langs: ['python'],
 });
+
+export function highlightCode(code: string) {
+	return highlighter.codeToHtml(code, {
+		lang: 'python',
+		theme: 'catppuccin-frappe',
+	});
+}
--- src/utils/highlightCode.ts
+++
@@ -1,8 +0,0 @@
-import { highlighter } from '../lib/shiki';
-
-export function highlightCode(code: string) {
-	return highlighter.codeToHtml(code, {
-		lang: 'python',
-		theme: 'catppuccin-frappe',
-	});
-}
