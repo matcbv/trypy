99ec5e5bd23dad2ff029ccd1157a1e78668a21f4
matcbv <matheuscbv23@gmail.com>
2026-09-13 00:12:38 -0300

    Adicao de comentario

--- src/types/richText.ts
+++ src/types/richText.ts
@@ -2,7 +2,7 @@ import type { Asset, Entry, EntrySkeletonType } from 'contentful';
 
 // * Criando tipos personalizados para nosso conteúdo bruto recebido do Contentful
 
-// Tipo personalizado para entries.
+// * Tipo personalizado para entries.
 export type ResolvedEntry<T extends EntrySkeletonType> = Entry<
 	T,
 	'WITHOUT_UNRESOLVABLE_LINKS',
