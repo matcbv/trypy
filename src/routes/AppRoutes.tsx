a93fe6d65917601c2306737c61d0c1dc1aa2fef7
matcbv <matheuscbv23@gmail.com>
2026-09-13 00:03:37 -0300

    Adicao do contentful provider ao react router

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -27,49 +27,55 @@ import { BeAPartner } from '../pages/BeAPartner';
 import { ScrollToTop } from '../components/ScrollToTop';
 import { guestMiddleware } from '../middlewares/guestMiddleware';
 import { SupportUs } from '../pages/SupportUs';
+import { ContentfulContentProvider } from '../contexts/ContentfulContentProvider';
 
 export function AppRoutes() {
 	return (
 		<BrowserRouter>
 			<ScrollToTop />
 			<AuthProvider>
-				<ProgressProvider>
-					<NavigationProvider>
-						<Routes>
-							<Route element={<BaseLayout />}>
-								<Route path="/" element={<Home />} />
-								<Route
-									element={<ProtectedRoute middlewares={[guestMiddleware]} />}
-								>
-									<Route path="/session" element={<Session />} />
-									<Route path="/register" element={<Register />} />
-								</Route>
-								<Route
-									element={<ProtectedRoute middlewares={[authMiddleware]} />}
-								>
-									<Route path="/dashboard" element={<Dashboard />}>
-										<Route index element={<UserOverview />} />
-										<Route path="profile" element={<EditProfile />} />
-										<Route path="support-us" element={<SupportUs />} />
-										<Route path="certifications" element={<Certifications />} />
-										<Route path="resolutions" element={<Resolutions />} />
+				<ContentfulContentProvider>
+					<ProgressProvider>
+						<NavigationProvider>
+							<Routes>
+								<Route element={<BaseLayout />}>
+									<Route path="/" element={<Home />} />
+									<Route
+										element={<ProtectedRoute middlewares={[guestMiddleware]} />}
+									>
+										<Route path="/session" element={<Session />} />
+										<Route path="/register" element={<Register />} />
 									</Route>
-									<Route element={<TerminalProvider />}>
-										<Route
-											path="/learning-path/:moduleId"
-											element={<Module />}
-										/>
+									<Route
+										element={<ProtectedRoute middlewares={[authMiddleware]} />}
+									>
+										<Route path="/dashboard" element={<Dashboard />}>
+											<Route index element={<UserOverview />} />
+											<Route path="profile" element={<EditProfile />} />
+											<Route path="support-us" element={<SupportUs />} />
+											<Route
+												path="certifications"
+												element={<Certifications />}
+											/>
+											<Route path="resolutions" element={<Resolutions />} />
+										</Route>
+										<Route element={<TerminalProvider />}>
+											<Route
+												path="/learning-path/:moduleId"
+												element={<Module />}
+											/>
+										</Route>
 									</Route>
+									<Route path="/learning-path" element={<LearningPath />} />
+									<Route path="/reset-password" element={<ResetPassword />} />
+									<Route path="/support" element={<Support />} />
+									<Route path="/about-us" element={<AboutUs />} />
+									<Route path="/be-a-partner" element={<BeAPartner />} />
 								</Route>
-								<Route path="/learning-path" element={<LearningPath />} />
-								<Route path="/reset-password" element={<ResetPassword />} />
-								<Route path="/support" element={<Support />} />
-								<Route path="/about-us" element={<AboutUs />} />
-								<Route path="/be-a-partner" element={<BeAPartner />} />
-							</Route>
-						</Routes>
-					</NavigationProvider>
-				</ProgressProvider>
+							</Routes>
+						</NavigationProvider>
+					</ProgressProvider>
+				</ContentfulContentProvider>
 			</AuthProvider>
 		</BrowserRouter>
 	);
