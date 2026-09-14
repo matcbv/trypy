594a609c682c273c4808d74130fb17dc763966db
matcbv <matheuscbv23@gmail.com>
2026-09-14 01:37:17 -0300

    Mudanca no padding do dashboard para mobile

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ export function Dashboard() {
 		<div className="flex w-full items-center justify-center">
 			<div className="relative my-30 flex w-full justify-center">
 				<DashboardNavbar />
-				<div className="bg-dashboard-background min-h-screen w-[min(600px,100%)] rounded-md p-10 shadow-[0_0_20px_#ffffff]/10 lg:rounded-tl-none">
+				<div className="bg-dashboard-background min-h-screen w-[min(600px,100%)] rounded-md p-7 shadow-[0_0_20px_#ffffff]/10 lg:rounded-tl-none lg:p-10">
 					<Outlet />
 				</div>
 			</div>
